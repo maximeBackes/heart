b2cc016b8cb7ee8c184960c9387d2fc9a9effe86
Vicky Zweistein <a.zweistein@gmx.com>
2021-05-11 18:47:55 +0200

    squelettre nav (menu) v2

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,32 +1,38 @@
-import {Link} from 'react-router-dom';
+import React from 'react';
+import '.Nav.css';<
 
 function Nav() {
     return (
-      <nav className="Nav">
-          <ul className="NavBarLink">
-            <li classeName="NavBarItem">
-              <a href="\" classeName="NavBarLink">
+      <nav className="navbar">
+          <ul className="navbarlinks">
+            <li classeName="navbaritem">
+              <a href="\" classeName="navbarlink">
                 Accueil
+                <img src="" />
               </a>
             </li>
-            <li classeName="NavBarItem">
-              <a href="\" classeName="NavBarLink">
+            <li classeName="navbaritem">
+              <a href="\recommandation" classeName="navbarlink">
                 Recommendation
+                <img src="" />
               </a>
             </li>
-            <li classeName="NavBarItem">
-              <a href="\" classeName="NavBarLinkCenter">
+            <li classeName="navbaritem">
+              <a href="\profil" classeName="navbarlinkcenter">
                 Profil
+                <img src="" />
               </a>
             </li>
-            <li classeName="NavBarItem">
-              <a href="\" classeName="NavBarLink">
+            <li classeName="navbaritem">
+              <a href="\traitement" classeName="navbarlink">
                 Traitement
+                <img src="" />
               </a>
             </li>
-            <li classeName="NavBarItem">
-              <a href="\" classeName="NavBarLink">
+            <li classeName="navbaritem">
+              <a href="\prendreRDV" classeName="navbarlink">
                 PrendreRDV
+                <img src="" />
               </a>
             </li>
           </ul>
