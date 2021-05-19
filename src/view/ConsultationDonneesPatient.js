5fc69494990ada39873e724099a6f0f4e4f2c1db
elise-per <45498502+elise-per@users.noreply.github.com>
2021-05-19 18:04:33 +0200

    Add files via upload

--- src/view/ConsultationDonneesPatient.js
+++ src/view/ConsultationDonneesPatient.js
@@ -5,7 +5,7 @@ function ConsultatioDonneesPatient() {
     return (
       <div className="ConsultationDonneesPatient">
         <img src={logo} alt="heart" height="42" width="50"/>
-        <h1>Consulter mes données</h1>
+        <h1 id="test">Consulter mes données</h1>
       </div>
     );
   }
--- src/view/RDVPatient.js
+++ src/view/RDVPatient.js
@@ -2,6 +2,7 @@ import logo from '../Logocoeur.png';
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import Button from '../components/Button';
 //import interactionPlugin from "@fullcalendar/interaction";
 
 
@@ -9,16 +10,26 @@ function RDVPatient() {
     return (
       <div className="RDVPatient">
         <img src={logo} alt="heart" height="42" width="50"/>
-        <h1>Prendre un rendez-vous</h1>
-        <FullCalendar
-        plugins={[ dayGridPlugin ]}
-        initialView="dayGridMonth"
-        weekends={false}
-        events={[
-          { title: 'event 1', date: '2019-04-01' },
-          { title: 'event 2', date: '2019-04-02' }
-        ]}
-        />
+        <section id="rdv">
+          <div>
+            <div>
+            <h1 id="test"> Prendre un rendez-vous </h1>
+            <form action="#" method="post">
+              <input id="calendrier" placeholder="Nom et Prénom" name="name" type="text"></input><br></br>
+              <br></br>
+              <input id="calendrier" placeholder="Adresse mail" name="email" type="email"></input><br></br>
+              <br></br>
+              <input id="calendrier" placeholder="Numéro de contact" name="number" type="number"></input><br></br>
+              <br></br>
+              <input id="calendrier" name="date" type="date" required=""></input><br></br>
+              <br></br>
+              <input id="calendrier" placeholder="Horaire souhaité" name="time" type="time"></input><br></br>
+              <br></br>
+              <Button label="Soumettre"></Button>
+            </form>
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
