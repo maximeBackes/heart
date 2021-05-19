306b289ab9206ae13a20da7f86a63cb499da6da0
elise-per <45498502+elise-per@users.noreply.github.com>
2021-05-19 17:15:58 +0200

    Add files via upload

--- src/view/ContacterMedecin.js
+++ src/view/ContacterMedecin.js
@@ -5,11 +5,11 @@ function ContacterMedecin() {
     return (
       <div className="ContacterMedecin">
         <img src={logo} alt="heart" height="42" width="50"/>
-        <h1>Contacter mon médecin</h1>
+        <h1 id="test">Contacter mon médecin</h1>
         <form action="#" method="post">
           <div><input id="a" type ="text" name="name" placeholder="Nom du destinataire"></input></div>
           <br></br>
-          <div><input id="message" name="message" placeholder="Ecrit..."></input></div>
+          <div><input id="message" name="message" placeholder="Message"></input></div>
         </form>
       </div>
     );
