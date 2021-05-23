23adc168119e53381b6a21f95b7cd30ee2f3ba51
maximeBackes <37797098+maximeBackes@users.noreply.github.com>
2021-05-23 21:57:07 +0200

    Update TableauDeBord.js

--- src/components/TableauDeBord.js
+++ src/components/TableauDeBord.js
@@ -2,10 +2,11 @@
 import React, { Component } from 'react';
 import Const from './InfoConstante';
 import Patient from '../model/patient';
-
+import axios from 'axios';
+import Activity from '../model/activity';
 
 const axios_instance = axios.create({
-    baseURL: ,
+    baseURL: 'https://api.fitbit.com/1/user/'+Activity.id+'-'+Patient.id+'/activities/heart',
     timeout: 1000
 });
 
@@ -14,11 +15,11 @@ export default class TableauDeBord extends Component {
     state = {
         titre: <h1>{Patient.fullname()}</h1>,
         Infos: [
-            { name: "Activité", value: {Patient} },
-            { name: "Fréquence cardiaque", value: "--" },
-            { name: "Pression artérielle", value: "--" },
-            { name: "Oxygène", value: "--" },
-            { name: "Glycémie", value: "--" }
+            { name: "Activité", value: {Activity.fromJSON(baseURL)} },
+            { name: "Fréquence cardiaque", value: {Patient.fromJSON(baseURL)} },
+            { name: "Pression artérielle", value: {Patient.fromJSON(baseURL} },
+            { name: "Oxygène", value: {Patient.fromJSON(baseURL} },
+            { name: "Glycémie", value: {Patient.fromJSON(baseURL} }
         ]
     }
 
